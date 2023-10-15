563b9f806922695b9a387e606972b3f97866f227
xmasuhai <xmasuhai@163.com>
2023-10-15 21:04:05 +0800

    初始化项目，简化入口文件

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { App } from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// const div = document.getElementById('root') as HTMLElement
+const div = document.getElementById('root')
+
+const root = ReactDOM.createRoot(div!)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
