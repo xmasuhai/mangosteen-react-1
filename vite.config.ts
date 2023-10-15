ce3a61b988c2cc4bc414c222ab6ae72b04f54b13
xmasuhai <xmasuhai@163.com>
2023-10-15 22:32:06 +0800

    change base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/mangosteen-react-1-preview/',
+  base: '/mangosteen-react-1-preview/',
   plugins: [react()],
 })
